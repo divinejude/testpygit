8d308c9557785b31524ae6e169bb7118392acf9f
OKEKE DIVINE <okekedivinejude@gmail.com>
2020-04-20 10:22:06 +0100

    CHECKING AND GRANTING ACCESS

--- Test.py
+++ Test.py
@@ -1,6 +1,15 @@
 print('HELLO\nWELCOME TO OUR SOFTWARE')
 username = (input('ENTER YOUR USERNAME:  '))
-password = int(input('ENTER YOUR PASSWORD:  '))
-
+#password = int(input('ENTER YOUR PASSWORD:  '))
+access='DIVINE'
+try:
+    if username== access :
+        password = int(input('ENTER YOUR PASSWORD:  '))
+        if password == 1234 :
+            print('YOU ARE WELCOME', username)
+    else:
+        print('invalid Username')
+except Exception as e:
+    print(e)
 
 
